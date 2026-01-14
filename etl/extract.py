4030f9a67ea301e07a8b8f885984af3f1f20a526
Felipe Bastos <felipe.bastosonaque@gmail.com>
2026-01-14 18:54:34 -0400

    adiciona script de extração de dados com leitura de arquivos CSV

---
+++ etl/extract.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import pandas as pd
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR / "data"
+RAW_DIR = DATA_DIR / "raw"
+
+vendas = pd.read_csv(RAW_DIR / "vendas.csv")
+clientes = pd.read_csv(RAW_DIR / "clientes.csv")
+produtos = pd.read_csv(RAW_DIR / "produtos.csv")
+
+print(vendas.head())
